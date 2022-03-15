2fad37acdbdaad2a8a573e5df6af6fcfa9c21b81
ivan_markov <zumkeee@gmail.com>
2022-03-15 17:48:56 +0400

    Hexlet

---
+++ src/main/java/hexlet/code/controllers/UrlCheckController.java
@@ -0,0 +1,59 @@
+package hexlet.code.controllers;
+
+import hexlet.code.domain.Url;
+import hexlet.code.domain.UrlCheck;
+import hexlet.code.domain.query.QUrl;
+import io.javalin.http.Handler;
+import kong.unirest.HttpResponse;
+import kong.unirest.Unirest;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+public class UrlCheckController {
+
+
+    public static Handler checkUrl = ctx -> {
+
+        long id = ctx.pathParamAsClass("id", Long.class).getOrDefault(null);
+
+        Url url = new QUrl()
+                .id.equalTo(id)
+                .findOne();
+
+        final HttpResponse<String> responseUrl;
+        try {
+            responseUrl = Unirest.get(url.getName())
+                    .asString();
+        } catch (Throwable e) {
+            ctx.sessionAttribute("flashError", e.getMessage());
+            ctx.redirect("/urls/" + id);
+            return;
+        }
+
+        int status = responseUrl.getStatus();
+        Document doc = Jsoup.parse(responseUrl.getBody());
+
+        String title = doc.title();
+
+        String description = "";
+        if (doc.select("meta[name=description]").size() != 0) {
+            description = doc.select("meta[name=description]")
+                    .first()
+                    .attr("content");
+        }
+
+        String h1 = "";
+        if (doc.select("h1").size() != 0) {
+            h1 = doc.select("h1")
+                    .first()
+                    .text();
+        }
+
+        UrlCheck urlCheck = new UrlCheck(status, title, h1, description, url);
+        urlCheck.save();
+
+        ctx.sessionAttribute("flash", "Сайт успешно проверен");
+        ctx.redirect("/urls/" + id);
+
+    };
+}
